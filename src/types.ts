97979678fe34d257409c8999903aa9dbd0eac678
MGB247 <ghayasbaig247@gmail.com>
2023-01-22 12:51:11 +0500

    chore: code cleaning, comments, readme and exclude src from package

--- src/types.ts
+++ src/types.ts
@@ -22,9 +22,21 @@ export interface ISQSQueueCreateOptions {
 }
 
 export interface IEmitOptions {
+  /**
+   * use with FIFO Topic/Queue to ensure the ordering of events
+   * For SQS client, refer to https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/using-messagegroupid-property.html
+   */
   partitionKey?: string;
+  /**
+   * Delay message for a specific time before which they can be
+   * processed. Specify in seconds.
+   * Max value is 900
+   */
   delay?: number;
-  retryCount?: number;
+  /**
+   * Set to true if using local emitter to emit on the 
+   * local node emitter
+   */
   useLocalEmitter?: boolean;
 }
 
@@ -48,15 +60,44 @@ export interface Queue {
 
 export interface Topic {
   name: string;
+  /**
+   * Set to true if topic is FIFO, default is false
+   */
   isFifo: boolean;
+  /**
+   * Set to true if you are consuming this topic
+   */
   isConsuming: boolean;
+  /**
+   * Optional prefix to differentiate topics from other
+   * services using same topic name
+   */
   servicePrefix?: string;
+  /**
+   * The time for which message won't be available to other
+   * consumers when it is received by a consumer
+   * Unit: s
+   * Default: 360s
+   */
   visibilityTimeout?: number;
+  /**
+   * Default: 10 for consumption
+   */
   batchSize?: number;
+  /**
+   * Maximum number the broker will attempt to retry the message
+   * before which it is added to the related DLQ if deadLetterQueueEnabled
+   * is true in emitter options
+   * Default: 3
+   */
   maxRetryCount?: number;
 }
 
 export interface IEventTopicMap {
+  /**
+   * Map events to topics.
+   * Multiple events maybe mapped to the same topic
+   */
   [eventName: string]: Topic;
 }
 
@@ -65,16 +106,54 @@ export enum EmitterType {
 }
 
 export interface IEmitterOptions {
+  /**
+   * Set to true if you are consuming any topic
+   */
   isConsumer?: boolean;
+  /**
+   * Map of events to topics
+   */
   eventTopicMap: IEventTopicMap;
+  /**
+   * Set to true if using external broker as client like SQS
+   */
   useExternalBroker?: boolean;
   emitterType: EmitterType;
+  /**
+   * Optional, to log slow messages
+   * Unit: ms
+   * Default: 60000ms
+   */
   maxProcessingTime?: number;
+  /**
+   * local, dev, stag, prod etc
+   */
   environment: string;
+  /**
+   * The local NodeJS Emitter used for logging failed events
+   */
   localEmitter: EventEmitter;
+  /**
+   * An optional event on which failed events will be emitted.
+   * These include failures when sending and consuming messages.
+   */
   eventOnFailure?: string;
+  /**
+   * Maximum number of times the broker will retry the message
+   * in case of failure in consumption after which it will be 
+   * moved to a DLQ if deadLetterQueueEnabled is true
+   * Default: 3
+   */
   maxRetries?: number;
+  /**
+   * Optional SQS Client config used by message producer
+   */
   sqsConfig?: SQSClientConfig
+  /**
+   * Set to true if you want to use DLQs
+   * Every topic will have a DLQ created against it that
+   * will be used when maxRetryCount is exceeded for a topic
+   */
   deadLetterQueueEnabled?: boolean;
 }
 
