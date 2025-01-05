443c00bc523e033d38d89a53dd57ab133332f194
Muhammad Ghayas Baig <engineer-0@Muhammads-MacBook-Pro-2.local>
2025-01-05 19:30:41 +0500

    feat(consumer-idempotency): code clean

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,12 +1,3 @@
 export enum DynamoTable {
   Idempotency = "event-broker-idempotency",
 }
-
-export interface DynamoTableStructure {
-  tableName: DynamoTable;
-  partitionKey: string;
-  sortKey?: string;
-  expiryKey?: string,
-  attributes: Record<string, string>;
-  expiry?: number;
-}
